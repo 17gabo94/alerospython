febeeedd94dc5ebcf3658d48ac4477934d846487
Gabriel Montaño <60579336+17gabo94@users.noreply.github.com>
2025-11-03 13:12:23 +0000

    add: password validator

--- password_validator/password_validator.py
+++ password_validator/password_validator.py
@@ -1,2 +1,16 @@
+import re
+
 def execute():
-    print("Has elegido la opción 2.")
+    newPassword = input("Enter you new password: ")
+    isValidPassword = validate_password(newPassword)
+    if(isValidPassword):
+        print("\nIt is a correct password!\n", isValidPassword)
+    else:
+        print("\nIt is not a correct password!\n", isValidPassword)
+    
+
+
+
+def validate_password(password):
+    pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$'
+    return bool(re.match(pattern, password))
