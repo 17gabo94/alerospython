e815f4d4171e009f3413f51a86acfc8d8ef05a40
Gabriel Montaño <60579336+17gabo94@users.noreply.github.com>
2025-11-03 17:25:40 +0000

    add: calculate balance

--- translation_log_calculator/translation_log_calculator.py
+++ translation_log_calculator/translation_log_calculator.py
@@ -1,2 +1,19 @@
 def execute():
-    print("Has elegido la opción 5.")
+    transaction_log = ["DEPOSIT,100", "WITHDRAW, 20", "DEPOSIT,50", "WITHDRAW,75"]
+    calculate_final_balance(transaction_log)
+
+def calculate_final_balance(transaction_log):
+    index = 0
+    totalBalance = 0
+    while(index < len(transaction_log)):
+        transaction = transaction_log[index]
+        transactionType = transaction.split(",")[0]
+        amount = int(transaction.split(",")[1])
+        if(transactionType == "DEPOSIT"):
+            totalBalance += amount
+        else:
+            totalBalance -= amount
+
+        index += 1
+
+    print("Final balance: ", totalBalance)
